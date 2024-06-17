7c7bf13014dfc9f72355ded3e82457901d4809a3
ankitkaushikx <ankitkaushikx@gmail.com>
2024-06-17 11:26:24 +0530

    Fixes , boilerplate ready

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,5 +5,13 @@
         </h2>
     </x-slot>
 
-    <livewire:transaction-form />
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+            <livewire:transaction-form />
+                </div>
+            </div>
+        </div>
+    </div>
 </x-app-layout>
--- resources/views/livewire/transaction-form.blade.php
+++ resources/views/livewire/transaction-form.blade.php
@@ -1,3 +1,3 @@
 <div>
-  Thi si sa 
+  
 </div>
